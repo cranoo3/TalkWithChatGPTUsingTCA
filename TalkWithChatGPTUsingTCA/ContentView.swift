dc8a2fa77a4783d409d06cecaa3d2070fa05894a
cranoo <75974473+cranoo3@users.noreply.github.com>
2024-10-04 00:56:07 +0900

    Initial Commit

---
+++ TalkWithChatGPTUsingTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TalkWithChatGPTUsingTCA
+//
+//  Created by cranoo on 2024/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TalkWithChatGPTUsingTCA/TalkWithChatGPTUsingTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkWithChatGPTUsingTCAApp.swift
+//  TalkWithChatGPTUsingTCA
+//
+//  Created by cranoo on 2024/10/04.
+//
+
+import SwiftUI
+
+@main
+struct TalkWithChatGPTUsingTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
